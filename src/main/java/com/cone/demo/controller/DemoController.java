053f365be58ac4a8552323a0f91e5e9daa6d2252
pactera <782291499@163.com>
2017-02-06 14:10:31 +0800

    add base packge

---
+++ src/main/java/com/cone/demo/controller/DemoController.java
@@ -0,0 +1,10 @@
+package com.cone.demo.controller;
+
+/**
+ * Created by pactera on 2017/2/6.
+ */
+public class DemoController {
+    public static void main(String []ss){
+        System.out.println("ssds");
+    }
+}
---
+++ src/main/java/com/cone/demo/dao/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by pactera on 2017/2/6.
+ */
+package com.cone.demo.dao;
