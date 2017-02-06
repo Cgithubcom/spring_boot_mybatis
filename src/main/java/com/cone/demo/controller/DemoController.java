ec6ce559d590b28a71610f7a40cb25a6ac02e2ab
pactera <782291499@163.com>
2017-02-06 15:03:11 +0800

    add springBoot

--- src/main/java/com/cone/demo/controller/DemoController.java
+++ src/main/java/com/cone/demo/controller/DemoController.java
@@ -1,10 +1,27 @@
 package com.cone.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by pactera on 2017/2/6.
  */
+@Controller
 public class DemoController {
-    public static void main(String []ss){
-        System.out.println("ssds");
+    @Value("${other.name}")
+    private String name;
+    @Value("${other.code}")
+    private String code;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!<br>name:"+name+"<br>code:"+code;
+    }
+    @RequestMapping("/test")
+    String test() {
+        return "test";
     }
 }
